6ba5dc280b0123d2bbab529e641fff88d36a2772
Kev <sulpeum@gmail.com>
2023-03-31 16:42:43 -0400

    styles: card styles

--- src/App.tsx
+++ src/App.tsx
@@ -130,7 +130,7 @@ function App() {
               setValidated(false)
             }}
           >
-            <CreateButtonSVG />
+            Add Job
           </Button>
         </nav>
 
@@ -173,22 +173,3 @@ function App() {
 }
 
 export default App
-
-const CreateButtonSVG = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    >
-      <line x1="12" y1="5" x2="12" y2="19"></line>
-      <line x1="5" y1="12" x2="19" y2="12"></line>
-    </svg>
-  )
-}
--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -17,13 +17,18 @@ export default function JobCard({ jobs, handleEditJob, handleDeleteJob }: Props)
             <Card.Title>{position}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">{company}</Card.Subtitle>
             <Card.Text>
-              {location} {remote} {status}
+              <div className="btn-extra">{location}</div>
+              {remote === "Yes" ? <div className="btn-extra">Remote</div> : null}
+              {remote === "No" ? <div className="btn-extra">On-site</div> : null}
+              {remote === "Hybrid" ? <div className="btn-extra">Hybrid</div> : null}
+
+              <div className="btn-extra">{status}</div>
             </Card.Text>
 
-            <Button className="btn-primary" onClick={() => handleEditJob(id)}>
+            <Button className="btn-secondary btn-test" onClick={() => handleEditJob(id)}>
               Edit
             </Button>
-            <Button className="btn-primary" onClick={() => handleDeleteJob(id)}>
+            <Button className="btn-primary btn-danger" onClick={() => handleDeleteJob(id)}>
               Delete
             </Button>
           </Card.Body>
